58bde7d9e1a8b11c2540373ee51339390ff3ae6a
Jethro Flanagan <jethroflanagan@gmail.com>
2023-07-11 22:50:19 +0200

    Denotched

--- src/Widget.jsx
+++ src/Widget.jsx
@@ -2,11 +2,15 @@ import styles from "./Widget.module.scss";
 import mountains from "./assets/widget-background.png";
 import { AnimatedLink } from './components/AnimatedLink';
 import { Phone } from "./components/Phone";
+import { StatusBar } from './components/StatusBar';
 import { WidgetLarge } from './components/WidgetLarge';
 
 export const Widget = () => {
   return (
     <Phone>
+      <div className={styles.statusContainer}>
+        <StatusBar />
+      </div>
       <div className={styles.widgetHeader}>
         <WidgetLarge />
         <div className={styles.mountains}>
--- src/components/StatusBar.jsx
+++ src/components/StatusBar.jsx
@@ -1,5 +1,5 @@
 import styles from './StatusBar.module.scss';
-import statusBar from "../assets/status-bar.png";
+import statusBar from "../assets/status-bar-notchless.png";
 
 export const StatusBar = () => (
   <div className={styles.statusBar}>
